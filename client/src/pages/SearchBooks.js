04bd2eddb55209fc5d1bab0ecfe4a103812a14e6
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-09-10 17:08:14 -0400

    add apollo to SavedBooks.js and SearchBooks.j, update App.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
+
+// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -13,6 +16,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -59,6 +64,8 @@ const SearchBooks = () => {
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    const [saveBook] = useMutation(SAVE_BOOK);
 
     if (!token) {
       return false;
